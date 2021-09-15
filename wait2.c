1251b19a4363967755b4999286504f4864c5798a
Vignesh Rao <vigneshravirao@gmail.com>
2021-09-15 17:57:43 +0530

    Update wait2 test

--- wait2.c
+++ wait2.c
@@ -4,19 +4,20 @@
 
 int main()
 {
-    int i=0;
-    
-    while(i<4){
+    int i = 0;
+
+    while (i < 2)
+    {
         i++;
-        int retime, rutime,stime;
+        int retime, rutime, stime;
         fork();
-
         int pid = wait2(&retime, &rutime, &stime);
-        if(pid==-1){
-            printf(1,"Process failed\n"); continue;
-        } 
-        printf(1,"Pid:%d Retime:%d STime:%d Rutime:%d\n",pid ,retime ,stime,rutime);
-
+        if (pid == -1)
+        {
+            printf(1, "No terminated child found for pid = %d\n", getpid());
+            continue;
+        }
+        printf(1, "parent pid=%d, child pid:%d Retime:%d STime:%d Rutime:%d\n", getpid(), pid, retime, stime, rutime);
     }
     exit();
 }
